65b03bd4aeea8db67f3f81e2fb5a65ab4544a4a4
ibra-zen <ibra-zen@users.noreply.github.com>
2026-09-04 13:31:51 +0300

    feat(android): add physical back button handler, multilingual i18n with English default Am I Stupid?, branded icons, and Play Store .aab release build

--- android_app/android/app/src/main/java/com/dh3games/acabasalakmiyim/MainActivity.java
+++ android_app/android/app/src/main/java/com/dh3games/acabasalakmiyim/MainActivity.java
@@ -1,13 +1,38 @@
 package com.dh3games.acabasalakmiyim;
 
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (this.getBridge() != null && this.getBridge().getWebView() != null) {
+            this.getBridge().getWebView().addJavascriptInterface(new Object() {
+                @JavascriptInterface
+                public void exitApp() {
+                    runOnUiThread(() -> finishAffinity());
+                }
+            }, "AndroidNative");
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.getBridge() != null && this.getBridge().getWebView() != null) {
+            this.getBridge().getWebView().post(() -> {
+                this.getBridge().getWebView().evaluateJavascript(
+                    "if (typeof handleAndroidBack === 'function') { handleAndroidBack(); }", 
+                    null
+                );
+            });
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
 
 
